36478e79e4d16f5e9a6af5ba5da70c9d8f06423d
Alexandr Kulik <alexandr.kulik1988@gmail.com>
2012-01-13 01:27:43 +0200

    Updated repository location

--- src/test/groovy/org/veil/gradle/plugins/jetty7/JettyPLuginTest.groovy
+++ src/test/groovy/org/veil/gradle/plugins/jetty7/JettyPLuginTest.groovy
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.instanceOf
 
 import static org.junit.Assert.assertThat
 import static org.junit.Assert.assertTrue
+import static org.hamcrest.CoreMatchers.is
 
 import org.gradle.api.Project
 import org.gradle.api.Task
